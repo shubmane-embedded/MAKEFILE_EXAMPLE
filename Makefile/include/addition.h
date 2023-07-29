447aa0ed91caaa3f276cd9b2b66700a128c4a280
Shubham Ramesh Mane <136062065+shubmane-embedded@users.noreply.github.com>
2023-07-29 11:59:19 +0530

    Update addition.h

--- Makefile/include/addition.h
+++ Makefile/include/addition.h
@@ -1,3 +1,15 @@
+/**
+ * Copyright (c) 2023
+ * All rights reserved.
+ *
+ * Author - Shubham Mane
+ * Date Created - 29 july 2023
+ * Last date Modified - 29 July 2023
+ * 
+ * @brief - header file
+ * @file - addition.h
+ */
+
 #ifndef __addition_h__
 #define __addition_h__
 
@@ -10,3 +22,5 @@
 int addition(int, int);
 
 #endif
+ 
+/* End of Code */
