f005a08d53f13776879d7c7dd151bae9ae159e19
Shubham Ramesh Mane <136062065+shubmane-embedded@users.noreply.github.com>
2023-07-29 12:16:39 +0530

    Update main.c

--- Makefile/source/main.c
+++ Makefile/source/main.c
@@ -1,9 +1,21 @@
-/*
+/**
+ * Copyright (c) 2023
+ * All rights reserved.
+ *
+ * Author - Shubham Mane
+ * Date Created - 29 July 2029
+ * Last date Modified - 29 July 2029
+ * 
+ * @brief - main source file
+ * @file - main.c
+ */
+
+/**
  * @Info - lib header file
  */
 #include <stdio.h>
 
-/*
+/**
  * @Info - Custom header file
  */
 #include "addition.h"
@@ -18,3 +30,5 @@ int main(void)
     printf("temp - %d\n", temp);
     return 0;
 }
+
+/* End of Code */
