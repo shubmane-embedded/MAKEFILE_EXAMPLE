9aa04d14dc8d7593e9c3304e9ec53131c0af2912
Shubham Ramesh Mane <136062065+shubmane-embedded@users.noreply.github.com>
2023-07-29 12:09:58 +0530

    Update addition.c

--- Makefile/source/addition.c
+++ Makefile/source/addition.c
@@ -1,6 +1,20 @@
+/**
+ * Copyright (c) 2023
+ * All rights reserved.
+ *
+ * Author - Shubham Mane
+ * Date Created - 29 July 2023
+ * Last date Modified - 29 July 2023
+ * 
+ * @brief - Source file
+ * @file - addition.h
+ */
+
 #include "../include/addition.h"
 
 int addition(int num_1, int num_2)
 {
     return (num_1 + num_2);
 }
+ 
+ /* End of Code */
