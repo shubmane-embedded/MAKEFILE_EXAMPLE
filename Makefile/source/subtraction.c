854f330220b66211edf2a4c1aa8b6335fc305328
Shubham Ramesh Mane <136062065+shubmane-embedded@users.noreply.github.com>
2023-07-29 12:13:40 +0530

    Update subtraction.c

--- Makefile/source/subtraction.c
+++ Makefile/source/subtraction.c
@@ -1,6 +1,20 @@
+/**
+ * Copyright (c) 2023
+ * All rights reserved.
+ *
+ * Author - Shubham Mane
+ * Date Created - 29 July 2023
+ * Last date Modified - 29 July 2023
+ * 
+ * @brief - Source file
+ * @file - subtraction.c
+ */
+
 #include "../include/subtraction.h"
 
 int subtraction(int num_1, int num_2)
 {
     return (num_1 - num_2);
 }
+ 
+ /* End of Code */
